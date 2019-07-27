374219f0b4c7928d48578ffb7d070721036f52df
OleksandrHlybchenko <logista@meta.ua>
2019-07-27 18:40:21 +0300

    Updates

---
+++ precache-manifest.01387c0a4a1c02346e4409f0d70406c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbf0ce3ad412a3043697fdc91079996",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "690d8aaae4236b280524",
+    "url": "/react_tabs/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "690d8aaae4236b280524",
+    "url": "/react_tabs/static/js/2.f3d5e3b3.chunk.js"
+  },
+  {
+    "revision": "83db4fcbbb7e677c7b7b",
+    "url": "/react_tabs/static/js/main.74d49ab2.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
